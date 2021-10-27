1e933b29b1b029ad4f7eaa1f34e9719b0a014ca1
Joaquín Velázquez Camacho <joaquinvelazquezcamacho@gmail.com>
2021-10-27 19:54:35 -0300

    initial commit

---
+++ wordpress/wp-content/themes/mentecompasiva-theme/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+	<?php wp_head(); ?>
+
+</head>
+<body>
+
+<button class="py-2 px-3 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700">
+    Click me 2
+</button>
+</body>
+</html>
