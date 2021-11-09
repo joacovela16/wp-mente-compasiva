22f83dd76aaf1afa633bb15dee945d6f14450864
Joaquín Velázquez Camacho <joaquinvelazquezcamacho@gmail.com>
2021-11-09 00:15:27 -0300

    avances en diseño

--- wordpress/wp-content/themes/mc-theme/assets/javascript/app/PostResume.js
+++ wordpress/wp-content/themes/mc-theme/assets/javascript/app/PostResume.js
@@ -6,7 +6,7 @@ const PostResume = {
             <div class="text-lg font-bold">Lorem Ipsum</div>
             <div class="flex-1"></div>
             <div class="cursor-pointer">
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-external-link" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                    <path d="M11 7h-5a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-5"></path>
                    <line x1="10" y1="14" x2="20" y2="4"></line>
