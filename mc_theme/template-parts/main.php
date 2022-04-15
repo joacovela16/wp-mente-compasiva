2a62bb89ff2ef922536c439c8207292cf1496768
Joaquín Velázquez Camacho <joaquinvelazquezcamacho@gmail.com>
2022-04-15 16:57:09 -0300

    improvements

--- mc_theme/template-parts/main.php
+++ mc_theme/template-parts/main.php
@@ -6,10 +6,10 @@ $logo_cm = get_template_directory_uri() . "/assets/images/logo.png";
 
 ?>
 
-<div class="relative overflow-hidden w-full">
+<div class="relative overflow-hidden w-full ">
 
-    <div style="height: calc(100vh - 88px)" class="relative w-full overflow-hidden">
-        <video class="filter blur-sm absolute top-0 left-0 w-full h-full object-cover" x-init="loaderOn=false" src="<?= $url ?>" autoplay muted loop></video>
+    <div style="height: calc(100vh - 88px)" class="relative w-full overflow-hidden ">
+        <video class="absolute top-0 left-0 w-full h-full object-cover gradient-animation" x-init="loaderOn=false" src="<?= $url ?>" autoplay muted loop></video>
         <div class="custom-shape-divider-bottom-1637812056 ">
             <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" class="-m-b-10" viewBox="0 0 1200 120" preserveAspectRatio="none">
                 <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z"
